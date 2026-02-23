aac62846d87041c85fb7d8be9052bc559706d1b2
doktorlich <sergei199522@mail.ru>
2026-02-23 18:09:57 +0300

    feat: add code snippet

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,37 +1,22 @@
 return {
   {
-    -- Указываем плагин, настройки которого хотим изменить
     "windwp/nvim-autopairs",
-    -- Используем функцию opts для расширения стандартных настроек LazyVim
     opts = function(_, opts)
-      -- Подгружаем модули плагина для создания новых правил и условий
       local Rule = require("nvim-autopairs.rule")
       local cond = require("nvim-autopairs.conds")
 
-      -- Убеждаемся, что список правил существует, и добавляем новое
       opts.rules = opts.rules or {}
+
       table.insert(
         opts.rules,
-        -- Создаем правило: при вводе "$" автоматически добавлять "}"
-        Rule("$", "}", {
-            -- Список языков, в которых это правило будет активно
-            "javascript",
-            "typescript",
-            "javascriptreact",
-            "typescriptreact",
-            "vue",
-            "svelte",
-          })
-          -- Условие: не срабатывать, если перед курсором уже есть "}"
+        Rule("$", "}", { "typescript", "javascript", "typescriptreact", "javascriptreact" })
+          -- Проверяем: если перед курсором символ `$`, то при его вводе (триггере)
+          -- мы превращаем это в `${}`
           :with_pair(
             cond.not_after_regex("}")
           )
-          -- Условие: срабатывать только если где-то раньше в строке есть обратная кавычка (backtick)
-          :with_pair(
-            cond.before_text("`")
-          )
-          -- Главная часть: заменяет стандартное поведение подстановки.
-          -- Вместо просто "}" после "$" мы вставляем "{}" и курсор окажется внутри скобок.
+          -- Убираем жесткую привязку к именам узлов Treesitter,
+          -- так как ваш парсер выдает общий `@string`
           :replace_endpair(
             function()
               return "{}"
